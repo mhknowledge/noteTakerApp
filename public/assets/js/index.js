da49f2cfad4e128d016701d9cc4aaadde3c66300
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-05-16 18:28:40 -0400

    fixes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,10 @@
-let noteTitle = $(".note-title");
-let noteText = $(".note-textarea"); 
-let saveNoteBtn = $(".save-note");
-let newNoteBtn = $(".new-note");
-let noteList = $(".list-container .list-group");
+let noteTitle;
+let noteText;
+let saveNoteBtn;
+let newNoteBtn;
+let noteList;
 
-if (window.location.pathname === './notes.html') {
+if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
