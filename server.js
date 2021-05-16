f7fe9b097a447854886fd87f0fa51103e3117f80
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-05-16 16:42:10 -0400

    sever.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+//dependencies:
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+//call express app:
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//data parsing:
+app.use(express.urlencoded({extended: true})):
+app.use(express.json());
+app.use(express.static(__dirname));
+require('./routes/routes')(app);
+
+//listener
+app.listen(PORT, function() {
+    console.log("Currently listening on comm port: " + PORT);
+});
