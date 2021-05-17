354f3627929ba43360b4fea4d60249456b93ce19
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-05-16 21:16:53 -0400

    new changes to index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,16 +1,16 @@
-let noteTitle;
-let noteText;
-let saveNoteBtn;
-let newNoteBtn;
-let noteList;
+let noteTitle = document.querySelector('.note-title');
+let noteText = document.querySelector('.note-textarea');
+let saveNoteBtn = document.querySelector('.save-note');
+let newNoteBtn = document.querySelector('.new-note');
+let noteList = document.querySelectorAll('.list-container .list-group');
 
-if (window.location.pathname === '/notes') {
-  noteTitle = document.querySelector('.note-title');
-  noteText = document.querySelector('.note-textarea');
-  saveNoteBtn = document.querySelector('.save-note');
-  newNoteBtn = document.querySelector('.new-note');
-  noteList = document.querySelectorAll('.list-container .list-group');
-}
+// if (window.location.pathname === '/notes') {
+//   noteTitle = document.querySelector('.note-title');
+//   noteText = document.querySelector('.note-textarea');
+//   saveNoteBtn = document.querySelector('.save-note');
+//   newNoteBtn = document.querySelector('.new-note');
+//   noteList = document.querySelectorAll('.list-container .list-group');
+// }
 
 // Show an element
 const show = (elem) => {
