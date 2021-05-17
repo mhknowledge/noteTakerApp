8f0ede9d6f13a54300dc4ed3fdee030d8b77e6f8
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-05-16 20:19:59 -0400

    request, response

--- routes/routes.js
+++ routes/routes.js
@@ -10,26 +10,26 @@ module.exports = app =>{
         //set up code for routes:
 
         //api get
-        app.get("/api/notes", function(req, res){
+        app.get("/api/notes", function(request, response){
             response.json(notes);
         });
         //api post
-        app.post("/api/notes", function(req, res){
+        app.post("/api/notes", function(request, response){
             let newItem = request.body;
             notes.push(newItem);
             updateDataBase();
             return console.log("Newest update: " + newItem.title);
         });
 
-        app.get("/api/notes/:id", function (req, res){
+        app.get("/api/notes/:id", function (request, response){
             response.json(notes[request.params.id]);
         });
 
-        app.get("/notes", function(req, res){
+        app.get("/notes", function(request, response){
             response.sendFile(path.join(__dirname, "../public/notes.html"));
         });
 
-        app.get('*', function(req, res){
+        app.get('*', function(request, response){
             response.sendFile(path.join(__dirname, "../public/index.html"));
         });
 
